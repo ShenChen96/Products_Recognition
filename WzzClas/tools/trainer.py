4de0240bd7c81b9d6fa6a29d86cc470db59fc2da
wzj <490316639@qq.com>
2021-06-24 19:23:47 +0800

    build training process

--- WzzClas/tools/trainer.py
+++ WzzClas/tools/trainer.py
@@ -1,3 +1,25 @@
 import torch
 
-device = torch.device("cuda")
+class Normal_Trainer():
+    def __init__(self, options, data_loader):
+        pass
+    def train_epoch(self):
+        pass
+    def train_step(self):
+        pass
+
+class Pair_Trainer():
+    def __init__(self, options, data_loader):
+        pass
+    def train_epoch(self):
+        pass
+    def train_step(self):
+        pass
+
+class Gan_Trainer():
+    def __init__(self, options, data_loader):
+        pass
+    def train_epoch(self):
+        pass
+    def train_step(self):
+        pass
